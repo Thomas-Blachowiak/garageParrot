f4f8266ebea5b8a28b6d70954799ad03ac9cf6ef
Thomas Blachowiak <blachowiak.thomas@outlook.fr>
2023-06-28 18:32:34 +0200

    Récupération des témoignages, pour les insérer dans les cards

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\TestimonialRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    /*public function index(): Response
     {
         return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }*/
+    #[Route('/', name: 'app_home')]
+    public function index (TestimonialRepository $testimonialRepository): Response
+    {
+        return $this->render('home/home.html.twig',[
+        'testimonial' => $testimonialRepository->findBy([],[])
         ]);
     }
 }
