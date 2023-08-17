f367c381693219dd6ef53a79d2818fd91c075fe5
root <root@vps-85578ef8.vps.ovh.net>
2023-08-17 11:27:33 +0000

    modification pour le sous-domaine

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build')
     // public path used by the web server to access the output path
-    .setPublicPath('/garageParrot/build')
+    .setPublicPath('/build')
     // only needed for CDN's or subdirectory deploy
     .setManifestKeyPrefix('build/')
 
